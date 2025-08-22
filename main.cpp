056c0de05504d6ff08cb5ee6896683892ebcdca0
oscar tyson <oscartysonshome@gmail.com>
2025-08-22 16:36:37 +1000

    test case

--- main.cpp
+++ main.cpp
@@ -1,1 +1,28 @@
 #include "main.h"
+
+
+
+int main(void) {
+	network exampleNetwork;
+
+	node hiddenNode(2, 0);
+	node inputLayerNode(0, 0);
+
+
+	layer inputLayer(2, std::make_shared<inputLayerNodeZero>);
+	inputLayer.allNodes()->getValueByIndex(0)->setNodeValue(0);
+	inputLayer.allNodes()->getValueByIndex(1)->setNodeValue(1);
+
+
+	layer hiddenLayer(2, std::make_shared<hiddenNode>);
+	layer outputLayer(1, std::make_shared<hiddenNode>);
+
+	exampleNetwork.setLayer(std::make_shared<layer>(inputLayer), 0);
+	exampleNetwork.setLayer(std::make_shared<layer>(hiddenLayer), 1);
+	exampleNetwork.setLayer(std::make_shared<layer>(outputLayer), 2);
+
+	propagate(std::make_shared<network>(exampleNetwork));
+
+
+
+}
